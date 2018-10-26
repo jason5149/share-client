8ba712bf40b1bba612bb0a0907286430e11ca29d
JJJJason <jason.tang880414@gmail.com>
2018-10-26 23:53:32 +0800

    change server port

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const app = express();
-const history = require('connect-history-api-fallback');
-const PORT = 4444;
+const express = require('express')
+const morgan = require('morgan')
 
-const staticFile = express.static('dist');
+const app = express()
+const history = require('connect-history-api-fallback')
 
-app.use(morgan());
-app.use(staticFile);
+const PORT = 3333
+
+const staticFile = express.static('dist')
+
+app.use(morgan())
+app.use(staticFile)
 app.use(history({
-  disableDotRule: true,
+  disableDotRule:    true,
   htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'],
-  logger: console.log.bind(console),
-}));
-app.use(staticFile);
+  logger:            console.log.bind(console),
+}))
+app.use(staticFile)
 
-app.listen(PORT, () => console.info(`Open http://localhost:${PORT}`));
+app.listen(PORT, () => console.info(`Open http://localhost:${ PORT }`))
