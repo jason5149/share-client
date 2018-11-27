2b343bd786841289cd9543acb8f2fe2f33d57e53
JJJJason <jason.tang880414@gmail.com>
2018-11-27 23:09:06 +0800

    add cluster_mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   apps: [
     {
-      name:   'share-client',
-      script: './server.js',
-      watch:  true,
-      env:    {
+      name:      'share-client',
+      script:    './server.js',
+      watch:     true,
+      exec_mode: 'cluster_mode',
+      env:       {
         'NODE_ENV': 'development',
       },
       env_production: {
